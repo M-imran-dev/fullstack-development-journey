72a8a58fbe0c5a8187d403ce786ed95ff122035e
Muhammad Imran <muhammadimran110965@gmail.com>
2026-09-25 04:35:38 +0500

    const,let,var variable practice

---
+++ JavaScript/01-basics/01-Variables.js
@@ -0,0 +1,13 @@
+const accountPassword = 11223344
+let accountName = "Muhammad Imran"
+var accountId = "123"
+/* we will never use var 
+    because its value change in different scope 
+*/
+accountEmail = "imran.contact@gmail.com";
+// also we will not use variable without const or let 
+// sami colon is not neccessary in javascript
+
+
+// console.log(accountPassword)
+console.table([accountPassword, accountName, accountId, accountEmail])
