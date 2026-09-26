705ccba62f7c6c8489b0dbbd1baa25fc6b1baf8c
Muhammad Imran <muhammadimran110965@gmail.com>
2026-09-27 00:14:32 +0500

    dataTypes

---
+++ JavaScript/02-dataTypes.js
@@ -0,0 +1,11 @@
+let name = "Muhammad Imran" // String dataType
+let age  = 19  // number datatype
+let loggedIn = true // boolean datatype 
+let loggedOut; // undefined because we have not asigned value 
+let temp = null // null 
+
+//bigint used for large numbers 
+
+// console.log(typeof "imran")
+console.table([typeof "Imran Dev", typeof "aged", typeof 134, typeof true,typeof undefined, typeof null])
+
